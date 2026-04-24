5b4c2d18c8802b8b1698ad1866d3d61269a292ca
Anvita <anvita192005@gmail.com>
2026-04-24 12:01:15 +0530

    api update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export const getZones = async (chapterId = null) => {
   const url = chapterId ? `${API_BASE}/zones?chapter_id=${chapterId}` : `${API_BASE}/zones`;
